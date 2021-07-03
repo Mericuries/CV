65bc4c7e8535675697e84039194f978b7ca60fbd
Mericuries <85179622+Mericuries@users.noreply.github.com>
2021-07-03 12:10:13 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+// let type = new Typed('.typing-text',{
+//     strings: ['web designer', 'front end developer', 'graphic designer', 'photographer'],
+//     typeSpeed: 120,
+//     loop: true,
+// })
